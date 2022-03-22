6a99a919e3d559bfb80460e9dfcdb7dd44bef6f5
Sebastian Kacperski <sebakacper123@gmail.com>
2022-03-22 19:25:48 +0100

    reaadme change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './index.css';
+
+let index = 0;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [isVisable, setIsVisable] = useState(null)
+  const [list, setList] = useState(null);
+  const [visableImg, setVisableImg] = useState(null);
+
+  useEffect(() => {
+    fetch('https://picsum.photos/v2/list')
+    .then(res => {
+      if(!res.ok){
+          throw new Error('Dane nie zostały pobrane z serwera');
+      }
+      return res.json();
+  }).then(json => {
+       setList(json);
+       setVisableImg(json.slice(index, 3))
+    })
+  }, [])
+
+  function next() {
+    if(index === 27){
+      index = 0;
+    } else { 
+      index = index+3 
+    }
+    setVisableImg(list.slice(index, index+3))
+  }
+ 
+  return ( 
+    <div className = "App" >
+      {isVisable && <div className='bg__single'><img key={ isVisable.id } className="single" src={isVisable.download_url} alt="photo"/><button onClick={()=> setIsVisable(null)} className="button">X</button></div>}
+      <div className="list">
+        {console.log(list)}
+            {visableImg && visableImg.map(item => (
+              <div className='list__box'><img key={ item.id } className="box__img" src={item.download_url} onClick={()=> setIsVisable(item)} alt="photo"/></div>
+            ))}
+      </div>
+      <button className="button" onClick={next}>Next</button>
     </div>
   );
 }
